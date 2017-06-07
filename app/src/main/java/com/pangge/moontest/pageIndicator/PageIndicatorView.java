fea5b505e53779034ae8ea3c701044bc32a427ce
iuuu <1410124787@qq.com>
2017-06-07 14:34:38 +0800

    viewpager commit

---
+++ app/src/main/java/com/pangge/moontest/pageIndicator/PageIndicatorView.java
@@ -0,0 +1,20 @@
+package com.pangge.moontest.pageIndicator;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by iuuu on 17/6/7.
+ */
+
+public class PageIndicatorView extends View {
+    public PageIndicatorView(Context context) {
+        super(context);
+    }
+
+    public PageIndicatorView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/pangge/moontest/viewpager/ViewPagerAdapter.java
@@ -0,0 +1,68 @@
+package com.pangge.moontest.viewpager;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by iuuu on 17/6/7.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter{
+    private List<View> viewList;
+
+    public ViewPagerAdapter() {
+        this.viewList = new ArrayList<>();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = viewList.get(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((View)object);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public int getCount() {
+        return viewList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public void setData(@Nullable List<View> list) {
+        this.viewList.clear();
+        if (list != null && !list.isEmpty()) {
+            this.viewList.addAll(list);
+        }
+
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    List<View> getData() {
+        if (viewList == null) {
+            viewList = new ArrayList<>();
+        }
+
+        return viewList;
+    }
+}
